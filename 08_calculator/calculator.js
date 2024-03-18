8e2a571566b33e3ce56ab7d1dd80210e5c4f24fe
Filip Smeets <32415661+filipsmeets@users.noreply.github.com>
2024-03-18 12:06:27 +0100

    Completed excersises 7 and 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array === TypeError) {
+    return 0;
+  } else {
+    return array.reduce((accumulator, currentValue) => accumulator + currentValue,0,);
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  if (array === TypeError) {
+    return 0;
+  } else {
+    return array.reduce((accumulator, currentValue) => accumulator * currentValue,);
+  }
 };
 
-const power = function() {
-	
+const power = function(num1,num2) {
+	return Math.pow(num1,num2)
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+	if (num1 === 0) {
+    return 1;
+  } else {
+    let fact = num1;
+    for (let index = (num1 - 1); index > 1; index--) {
+      fact = fact * index;
+    }
+    return fact;
+  }
 };
 
 // Do not edit below this line
