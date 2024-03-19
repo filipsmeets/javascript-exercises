f4a5b9aed9728f350fb65d005b5f4786b08cb547
Filip Smeets <32415661+filipsmeets@users.noreply.github.com>
2024-03-19 08:17:39 +0100

    Completed excersise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,13 @@
-const palindromes = function () {
+const palindromes = function (txt) {
+
+    const txtClean = txt.toLowerCase().replace((/,|\.|!| /g),'');
+    const reverseTxt = txt.toLowerCase().replace((/,|\.|!| /g),'').split('').reverse().join('');
+    
+    if (txtClean === reverseTxt) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
