0b9bfdda36205c42ccbca845ca2e025bf20a1eb3
Filip Smeets <32415661+filipsmeets@users.noreply.github.com>
2024-03-24 10:11:08 +0100

    Solved excercise 10 until test with fibonacci number 0

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,29 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+    // The following number is the sum of the previous two numbers
+    // 1,1,2,3,5,8,13,21,34
+    
+    let nr1 = 0;
+    let nr2 = 1;
+    let nr3 = nr1 + nr2;
+    const serie = [nr1, nr2, nr3];
+    
+    do {
+       
+        nr1 = nr2;
+        console.log(nr1);
+       
+        nr2 = nr3;
+        console.log(nr3);
+
+        nr3 = nr1 + nr2;
+        console.log(nr3);
+
+        serie.push(nr3);
+        console.log(serie);
+
+    } while (serie.length <= member -1);
+
+    return serie[serie.length-1];
 
 };
 
