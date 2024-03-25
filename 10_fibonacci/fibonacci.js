6878e870208e4a9a03e2a61d044b4b9d08b90191
Filip Smeets <32415661+filipsmeets@users.noreply.github.com>
2024-03-25 08:19:24 +0100

    Fixed the code in excercise 10 to also accept strings

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,30 +1,19 @@
 const fibonacci = function(member) {
-    // The following number is the sum of the previous two numbers
-    // 1,1,2,3,5,8,13,21,34
     
-    let nr1 = 0;
-    let nr2 = 1;
-    let nr3 = nr1 + nr2;
-    const serie = [nr1, nr2, nr3];
+    const serie = [0,1];
+
+    if (Number(member) < 0) {
+        return 'OOPS';
+    } else if (Number(member) < 2) {
+        return serie[member];
+    } else {
+        do {
+            serie.push(serie[serie.length - 2] + serie[serie.length - 1]);
+        } while (serie.length <= member);
+
+        return serie[serie.length - 1];
+    }
     
-    do {
-       
-        nr1 = nr2;
-        console.log(nr1);
-       
-        nr2 = nr3;
-        console.log(nr3);
-
-        nr3 = nr1 + nr2;
-        console.log(nr3);
-
-        serie.push(nr3);
-        console.log(serie);
-
-    } while (serie.length <= member -1);
-
-    return serie[serie.length-1];
-
 };
 
 // Do not edit below this line
