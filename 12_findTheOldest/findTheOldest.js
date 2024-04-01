0daf299da110f29e290ab8405679f1b63b13ff43
Filip Smeets <32415661+filipsmeets@users.noreply.github.com>
2024-04-01 17:01:58 +0200

    Completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
 
+    const oldest = array.reduce((person1, person2) => {
+        
+        const currentYear = (new Date(Date.now())).getFullYear();
+        if (person1.yearOfDeath === undefined) {
+            person1.yearOfDeath = currentYear;
+            if (person2.yearOfDeath === undefined) {
+                person2.yearOfDeath = currentYear;
+            }
+        }
+    
+        if (person1.yearOfDeath - person1.yearOfBirth > person2.yearOfDeath - person2.yearOfBirth) {
+            return person1;
+        } else {
+            return person2;
+        }
+
+    })
+
+    return oldest;
 };
 
 // Do not edit below this line
