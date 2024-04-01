d5e2916202252647f08b240f3d4022b0ac8ad760
Filip Smeets <32415661+filipsmeets@users.noreply.github.com>
2024-04-01 12:17:23 +0200

    Solved exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,13 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
 
-};
+    const titles = []
+    books.forEach(element => {
+        titles.push(element.title); 
+    });
+    console.log(titles);
+    return titles;
+
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
